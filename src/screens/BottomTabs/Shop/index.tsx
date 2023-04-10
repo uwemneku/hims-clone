c95aeb52c2f7e716a882d3b8885892440e195c35
Uwem Israel <iamuwem@gmail.com>
2023-04-10 09:59:52 +0100

    fixing folder naming

--- src/screens/BottomTabs/Shop/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as ShopNavigatorHeader } from "./components/Header";
-export { default as MaterialTopScrollContextProvider } from "./components/ScrollContext";
-export { default as ShopFeaturesScreen } from "./Features";
-export { default as ShopBrowseScreen } from "./Browse";
--- src/screens/BottomTabs/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as BottomTabs_HomeScreen } from "./home";
-export { default as BottomTabs_CareScreen } from "./care";
-export { default as BottomTabs_ProgramsScreen } from "./Programs";
-export { default as BottomTabs_TabBar } from "./components/TabBar";
